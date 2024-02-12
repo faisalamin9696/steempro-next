b105b44e1966dcb08713ce7b0d1bc9f0ff519081
Muhammad Faisal Amin <53206866+faisalamin9696@users.noreply.github.com>
2024-02-12 15:41:39 +0500

    fix: auth secret added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials"
 
 
 const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: 'jwt',
     },
