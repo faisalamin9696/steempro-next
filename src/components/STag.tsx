2b13324b7a32dd5cefa3a3e803f25ca08336f19c
Muhammad Faisal Amin <53206866+faisalamin9696@users.noreply.github.com>
2024-04-14 16:08:08 +0500

    ref: STag href fixes

--- src/components/STag.tsx
+++ src/components/STag.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import { validateCommunity } from '@/libs/utils/helper';
 
 export default function STag({ className, content, tag }: { className?: string, content?: string, tag: string }) {
-    return (<Link title={tag} className={clsx(className, 'text-default-900 ')} href={`/trending'}/${tag}`} >
+    return (<Link title={tag} className={clsx(className, 'text-default-900 ')} href={`/trending/${tag}`} >
         {content ?? tag}
     </Link >
     )
