824edc67c9c192145a9f8f49763a51bfe07936c7
Muhammad Faisal Amin <53206866+faisalamin9696@users.noreply.github.com>
2024-05-04 08:38:48 +0500

    ref: firebase credentials changes

--- src/libs/firebase/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBdobL0PyGeqJIRP2kF7VIFcz89pYP5QbQ",
-    authDomain: "steempro-blogs.firebaseapp.com",
-    projectId: "steempro-blogs",
-    storageBucket: "steempro-blogs.appspot.com",
-    messagingSenderId: "16233284467",
-    appId: "1:16233284467:web:cdccf991054e84801a7b06",
-    measurementId: "G-G18X0B2QSX"
-};
