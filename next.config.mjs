9518a879e42b729f1843398e89af96a8962210bf
faisalamin9696 <faisalamin9696@gmail.com>
2024-06-28 23:29:33 +0500

    ref: next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            // {
-            //     protocol: 'https',
-            //     hostname: 'cdn.steemitimages.com',
-            //     port: '',
-            // },
-            // {
-            //     protocol: 'https',
-            //     hostname: 'steemitimages.com',
-            //     port: '',
-            // },
-            {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-            },
+  // output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      // {
+      //     protocol: 'https',
+      //     hostname: 'cdn.steemitimages.com',
+      //     port: '',
+      // },
+      // {
+      //     protocol: 'https',
+      //     hostname: 'steemitimages.com',
+      //     port: '',
+      // },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+      },
 
-            {
-                protocol: 'http',
-                hostname: '**',
-                port: '',
-            },
-        ],
-    },
-    env: {
-        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
-    }
+      {
+        protocol: "http",
+        hostname: "**",
+        port: "",
+      },
+    ],
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    NEXT_DB_HOST: process.env.NEXT_DB_HOST,
+    NEXT_DB_PORT: process.env.NEXT_DB_PORT,
+    NEXT_DB_USERNAME: process.env.NEXT_DB_USERNAME,
+    NEXT_DB_PASSWORD: process.env.NEXT_DB_PASSWORD,
+    NEXT_DB_DATABASE: process.env.NEXT_DB_DATABASE,
+    NEXT_SSH_HOST: process.env.NEXT_SSH_HOST,
+    NEXT_SSH_PORT: process.env.NEXT_SSH_PORT,
+    NEXT_SSH_USERNAME: process.env.NEXT_SSH_USERNAME,
+    NEXT_SSH_PASSWORD: process.env.NEXT_SSH_PASSWORD,
+  },
 };
 
 export default nextConfig;
