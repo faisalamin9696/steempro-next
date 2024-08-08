8319e39f10c943980347446a7b97bfe5dcea2196
faisalamin9696 <faisalamin9696@gmail.com>
2024-08-08 16:01:23 +0500

    dev: rpc change fixes

---
+++ src/app/tools/app-usage/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { awaitTimeout } from "@/libs/constants/AppFunctions";
+import { getActiveFeed } from "@/libs/steem/sds";
+import React, { useEffect } from "react";
+
+function page() {
+  let allPosts: Feed[] = [];
+  let apps: { name: string; count?: number }[] = [];
+  let appCounts = {};
+
+  useEffect(() => {
+    // getAllPosts();
+  }, []);
+
+  async function getAllPosts() {
+    try {
+      allPosts = [];
+      for (let index = 0; index < 40; index++) {
+        const posts = await getActiveFeed(
+          "trending",
+          "null",
+          50,
+          1000,
+          1000 * (index || 1)
+        );
+
+        allPosts.push(...posts);
+        awaitTimeout(1);
+        if (posts?.length < 1000) {
+          break;
+        }
+      }
+      if (allPosts.length) {
+        countApps(allPosts);
+      }
+    } catch (error) {
+    } finally {
+      allPosts = [];
+    }
+  }
+
+  function countApps(posts) {
+    apps = [];
+    appCounts = {};
+    posts.forEach((element) => {
+      const json_metadata = JSON.parse(element?.["json_metadata"] ?? `{}`);
+      const app = json_metadata?.["app"];
+
+      if (app) {
+        if (appCounts[app]) {
+          appCounts[app].count += 1;
+        } else {
+          appCounts[app] = { name: app, count: 1 };
+        }
+      }
+    });
+
+    apps = Object.values(appCounts);
+  }
+  return <div>App Usage</div>;
+}
+
+export default page;
