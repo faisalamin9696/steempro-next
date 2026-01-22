01679924f94eb0a46ec41943538a40730f33fc67
faisalamin9696 <faisalamin9696@gmail.com>
2026-01-22 22:17:32 +0500

    fix: auth fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,3 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-import { validateHost } from "@/utils/helper";
-import { checkBotId } from "botid/server";
-import { NextRequest, NextResponse } from "next/server";
+import { handlers } from "@/auth";
 
-export async function POST(req: NextRequest) {
-  try {
-    const verification = await checkBotId();
-
-    if (verification.isBot || !validateHost(req.headers.get("host"))) {
-      return NextResponse.json({ error: "Access denied" }, { status: 403 });
-    }
-    return handlers.POST(req);
-  } catch (error) {
-    return NextResponse.error();
-  }
-}
-
-export async function GET(req: NextRequest) {
-  try {
-    const verification = await checkBotId();
-
-    if (verification.isBot || !validateHost(req.headers.get("host"))) {
-      return NextResponse.json({ error: "Access denied" }, { status: 403 });
-    }
-    return handlers.GET(req);
-  } catch (error) {
-    return NextResponse.error();
-  }
-}
+export const { GET, POST } = handlers;
