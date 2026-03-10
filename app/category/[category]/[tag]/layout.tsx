f786caa833fa75ae569324468f687da5ce95d290
faisalamin9696 <faisalamin9696@gmail.com>
2026-03-10 11:24:09 +0500

    fix: category page fixes

--- app/category/[category]/[tag]/layout.tsx
+++ app/category/[category]/[tag]/layout.tsx
@@ -1,85 +1,24 @@
 import MainWrapper from "@/components/wrappers/MainWrapper";
-import { sdsApi } from "@/libs/sds";
-import { Suspense } from "react";
-import ProfileHeaderSkeleton from "@/components/skeleton/ProfileHeaderSkeleton";
-import { auth } from "@/auth";
-import CommunityCard from "@/components/community/CommunityCard";
-import CommunityPage from "./page";
-import CommunityHeader from "@/components/community/CommunityHeader";
-import { getThumbnail } from "@/utils/image";
-import moment from "moment";
-import { Metadata, ResolvingMetadata } from "next";
 import { getMetadata } from "@/utils/metadata";
+import { Metadata } from "next";
 
-async function layout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: Promise<{ tag: string }>;
-}) {
-  const { tag } = await params;
-  const session = await auth();
-  const [[account, community], pinnedPosts] = await Promise.all([
-    Promise.all([
-      sdsApi.getAccountExt(`hive-${tag}`, session?.user?.name),
-      sdsApi.getCommunity(`hive-${tag}`, session?.user?.name),
-    ]),
-    sdsApi.getCommunityPinnedPosts(`hive-${tag}`, session?.user?.name),
-  ]);
-
-  return (
-    <Suspense fallback={<ProfileHeaderSkeleton />}>
-      <MainWrapper
-        endClass="w-[320px] min-w-[320px] 1md:hidden! lg:block!"
-        end={
-          <CommunityCard
-            account={account}
-            community={community}
-            className="card"
-          />
-        }
-        className="mt-2"
-      >
-        <CommunityHeader account={account} community={community} />
-        <CommunityPage
-          account={account}
-          community={community}
-          pinnedPost={(pinnedPosts ?? [])?.map((item) => {
-            return {
-              author: item.author,
-              permlink: item.permlink,
-              title: item.title,
-              thumbnail: getThumbnail(item.json_images, "640x0")!,
-              id: item.link_id.toString(),
-              created_at: moment.unix(item.created).toISOString(),
-            };
-          })}
-        />
-      </MainWrapper>
-    </Suspense>
-  );
+async function layout({ children }: { children: React.ReactNode }) {
+  return <MainWrapper>{children}</MainWrapper>;
 }
 
 export default layout;
 
-export async function generateMetadata(
-  { params }: { params: Promise<{ category: string; tag: string }> },
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  let { tag, category } = await params;
-  const { title, description, keywords, images } =
-    await getMetadata.communityAsync(category, tag, parent);
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  let { category, tag } = await params;
+  const { title, description, keywords, alternates } = getMetadata.category(
+    category,
+    tag,
+  );
 
   return {
     title,
     description,
     keywords: keywords.join(", "),
-    openGraph: {
-      images: [...images],
-    },
-    twitter: {
-      images: [...images],
-    },
+    alternates,
   };
 }
--- app/category/[category]/[tag]/page.tsx
+++ app/category/[category]/[tag]/page.tsx
@@ -1,47 +1,57 @@
 "use client";
 
-import CommunityCard from "@/components/community/CommunityCard";
-import CommuntiyLog from "@/components/community/CommuntiyLog";
-import STabs from "@/components/ui/STabs";
-import { addCommunityHandler } from "@/hooks/redux/reducers/CommunityReducer";
-import { useAppDispatch, useAppSelector } from "@/hooks/redux/store";
-import { useDeviceInfo } from "@/hooks/redux/useDeviceInfo";
 import { getMetadata, updateMetadata } from "@/utils/metadata";
-import { Accordion, AccordionItem } from "@heroui/accordion";
-import { TrendingUp, Sparkles, ClockPlus, Logs } from "lucide-react";
 import { useSession } from "next-auth/react";
+import { Key, useState } from "react";
 import { useParams } from "next/navigation";
-import { Key, useEffect, useMemo, useState } from "react";
+import STabs from "@/components/ui/STabs";
+import { Zap, TrendingUp, ClockPlus, DollarSign, Sparkles } from "lucide-react";
+import { useDeviceInfo } from "@/hooks/redux/useDeviceInfo";
 import { FeedList } from "@/components/FeedList";
-import HomeCarousal from "@/components/carousal/HomeCarousal";
-
 const ICON_SIZE = 20;
 
-function CommunityPage({
-  community,
-  account,
-  pinnedPost,
-}: {
-  community: Community;
-  account: AccountExt;
-  pinnedPost: PromotedPost[];
-}) {
-  const { category, tag } = useParams() as { category: string; tag: string };
-  const initialTab = category ?? "trending";
-  const [selectedKey, setSelectedKey] = useState(initialTab);
+function CategoryPage() {
+  const { category, tag } = useParams();
   const { data: session } = useSession();
-  const { useSmaller } = useDeviceInfo();
-  const isMobile = useSmaller("sm");
-  const dispatch = useAppDispatch();
+  const [selectedKey, setSelectedKey] = useState(
+    (category as string) || "trending",
+  );
+  const { isMobile } = useDeviceInfo();
+  const apiParam = `/${tag}`;
 
-  const communityData =
-    useAppSelector(
-      (state) => state.communityReducer.values[community.account],
-    ) ?? community;
+  const homeTabs = [
+    {
+      id: "trending",
+      title: "Trending",
+      api: "getActivePostsByTagTrending" + apiParam,
+      icon: <TrendingUp size={ICON_SIZE} />,
+    },
+    {
+      id: "popular",
+      title: "Popular",
+      api: "getActivePostsByTagInteraction" + apiParam,
+      icon: <Sparkles size={ICON_SIZE} />,
+    },
+    {
+      id: "created",
+      title: "Recent",
+      api: "getActivePostsByTagCreated" + apiParam,
+      icon: <ClockPlus size={ICON_SIZE} />,
+    },
+    {
+      id: "hot",
+      title: "Hot",
+      api: "getActivePostsByTagHot" + apiParam,
+      icon: <Zap size={ICON_SIZE} />,
+    },
 
-  useEffect(() => {
-    if (community) addCommunityHandler(community);
-  }, [community, dispatch]);
+    {
+      id: "payout",
+      title: "Payout",
+      api: "getActivePostsByTagPayout" + apiParam,
+      icon: <DollarSign size={ICON_SIZE} />,
+    },
+  ];
 
   const handleSelectionChange = (key: Key) => {
     if (!key) return;
@@ -50,94 +60,28 @@ function CommunityPage({
     updateMetadata({ title, description });
   };
 
-  const apiParams = `/${community.account}`;
-
-  const communityTabs = useMemo(
-    () => [
-      {
-        id: "trending",
-        title: "Trending",
-        api: "getActiveCommunityPostsByTrending" + apiParams,
-        icon: <TrendingUp size={ICON_SIZE} />,
-      },
-
-      {
-        id: "popular",
-        title: "Popular",
-        api: "getActiveCommunityPostsByInteraction" + apiParams,
-        icon: <Sparkles size={ICON_SIZE} />,
-      },
-      {
-        id: "created",
-        title: "Recent",
-        api: "getCommunityPostsByCreated" + apiParams,
-        icon: <ClockPlus size={ICON_SIZE} />,
-      },
-
-      {
-        id: "log",
-        title: "Activities",
-        icon: <Logs size={ICON_SIZE} />,
-        children: <CommuntiyLog account={community.account} />,
-      },
-    ],
-    [apiParams, isMobile],
-  );
-
   return (
-    <div className="flex flex-col gap-2">
-      <Accordion
-        isCompact
-        variant="bordered"
-        className="border-1 block lg:hidden"
-        itemClasses={{
-          title: "text-muted",
-          indicator: "text-foreground text-muted",
-        }}
-      >
-        <AccordionItem key="profile" aria-label="Profile details" title="About">
-          <CommunityCard
-            community={communityData}
-            account={account}
-            classNames={{
-              base: "shadow-none! border-none! bg-transparent",
-              body: "pt-0!",
-            }}
-          />
-        </AccordionItem>
-      </Accordion>
-      {pinnedPost?.length > 0 && (
-        <div className="mt-3">
-          <HomeCarousal data={pinnedPost} showPagination />
+    <STabs
+      key={`tabs-category-${session?.user?.name || "anonymous"}`}
+      variant="bordered"
+      selectedKey={selectedKey}
+      onSelectionChange={handleSelectionChange}
+      items={homeTabs}
+      tabTitle={(tab) => (
+        <div className="flex items-center space-x-2">
+          {tab.icon}
+          {!isMobile || selectedKey === tab.id ? (
+            <span>{tab.title}</span>
+          ) : null}
         </div>
       )}
-      <STabs
-        key={`tabs-community-${session?.user?.name || "anonymous"}`}
-        variant="bordered"
-        selectedKey={selectedKey}
-        onSelectionChange={handleSelectionChange}
-        items={communityTabs}
-        className={pinnedPost.length > 0 ? "" : "md:mt-3"}
-        tabHref={(tab) => `/${tab.id}/${community.account}`}
-        tabTitle={(tab) => (
-          <div className="flex items-center space-x-2">
-            {tab.icon}
-            {!isMobile || selectedKey === tab.id ? (
-              <span>{tab.title}</span>
-            ) : null}
-          </div>
-        )}
-      >
-        {(tab) =>
-          tab.children ? (
-            tab.children
-          ) : (
-            <FeedList apiPath={tab.api} observer={session?.user?.name} />
-          )
-        }
-      </STabs>
-    </div>
+      tabHref={(tab) => `/${tab.id}/${tag}`}
+    >
+      {(tab) => (
+        <FeedList apiPath={tab.api} observer={session?.user?.name ?? "steem"} />
+      )}
+    </STabs>
   );
 }
 
-export default CommunityPage;
+export default CategoryPage;
