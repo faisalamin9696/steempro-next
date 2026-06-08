6f8082807977bbec45fb0be3270981a4114ea551
faisalamin9696 <faisalamin9696@gmail.com>
2026-06-08 14:02:49 +0500

    bump: nextjs, axios version update

--- proxy.ts
+++ proxy.ts
@@ -42,11 +42,11 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
   ],
 };
